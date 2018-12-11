0abd0a17b24dd18d33776ca49ded335234c90088
Jisse Reitsma <jisse@yireo.com>
2018-12-11 10:26:26 +0100

    Remove dependency

--- source/app/code/community/Yireo/TaxRatesManager/Provider/OnlineRates.php
+++ source/app/code/community/Yireo/TaxRatesManager/Provider/OnlineRates.php
@@ -21,6 +21,21 @@ class Yireo_TaxRatesManager_Provider_OnlineRates
     private $config;
 
     /**
+     * @var array
+     */
+    private $columns = [
+        'code' => 'Code',
+        'country' => 'Country',
+        'state' => 'State',
+        'zip' => 'Zip/Post Code',
+        'rate' => 'Rate',
+        'zip_is_range' => 'Zip/Post is Range',
+        'range_from' => 'Range From',
+        'range_to' => 'Range To',
+        'default' => 'default'
+    ];
+
+    /**
      * Yireo_TaxRatesManager_Provider_OnlineRates constructor.
      * @param Yireo_TaxRatesManager_Config_Config $config
      */
@@ -33,24 +48,16 @@ class Yireo_TaxRatesManager_Provider_OnlineRates
     /**
      * @return Yireo_TaxRatesManager_Rate_Rate[]
      * @throws Mage_Core_Model_Store_Exception
+     * @throws Zend_Cache_Exception
      */
     public function getRates(): array
     {
         require_once BP . '/vendor/autoload.php';
 
         $rates = [];
-        $onlineRates = new Yireo\EuVatRates\MagentoRates(BP . '/var/cache');
+        $onlineRates = $this->getRatesFromCacheOrOnline();
 
-        if ($this->config->allowCache() === false) {
-            $onlineRates->disableCache();
-        }
-
-        $onlineUrl = $this->config->getFeedUrl();
-        if (!empty($onlineUrl)) {
-            $onlineRates->setPath($onlineUrl);
-        }
-
-        foreach ($onlineRates->getRates() as $onlineRate) {
+        foreach ($onlineRates as $onlineRate) {
             $rates[] = new Yireo_TaxRatesManager_Rate_Rate(
                 0,
                 (string)$onlineRate['code'],
@@ -60,5 +67,117 @@ class Yireo_TaxRatesManager_Provider_OnlineRates
         }
 
         return $rates;
+    }
+
+    /**
+     * @return array
+     * @throws Mage_Core_Model_Store_Exception
+     * @throws Zend_Cache_Exception
+     */
+    private function getRatesFromCacheOrOnline(): array
+    {
+        if ($this->config->allowCache()) {
+            $rates = $this->loadFromCache();
+            if (!empty($rates)) {
+                return $rates;
+            }
+        }
+
+        $rates = $this->loadFromOnline();
+        $this->saveToCache($rates);
+
+        return $rates;
+    }
+
+    /**
+     * @return array
+     * @throws Mage_Core_Model_Store_Exception
+     */
+    private function loadFromCache(): array
+    {
+        if ($data = Mage::app()->getCache()->load($this->getCacheId())) {
+            $data = unserialize($data);
+        }
+
+        if (!empty($data) && is_array($data)) {
+            return $data;
+        }
+
+        return [];
+    }
+
+    /**
+     * @return array
+     * @throws Mage_Core_Model_Store_Exception
+     */
+    private function loadFromOnline(): array
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$this->config->getFeedUrl());
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $contents = curl_exec($ch);
+        curl_close ($ch);
+
+        $rows = array_map('str_getcsv', explode("\n", $contents));
+        $headerRow = array_shift($rows);
+        $this->validateHeaderRow($headerRow);
+
+        foreach ($rows as $row) {
+            if (empty($row[0])) {
+                continue;
+            }
+            $i = 0;
+            $rate = [];
+            foreach ($this->columns as $columnCode => $columnName) {
+                $rate[$columnCode] = $row[$i] ?? '';
+                $i++;
+            }
+            $rates[] = $rate;
+        }
+
+        return $rates;
+    }
+
+    /**
+     * @return string
+     * @throws Mage_Core_Model_Store_Exception
+     */
+    private function getCacheId(): string
+    {
+        $feedUrl = $this->config->getFeedUrl();
+        return 'TAXRATESMANAGER_' . md5($feedUrl);
+    }
+
+    /**
+     * @param $data
+     * @throws Mage_Core_Model_Store_Exception
+     * @throws Zend_Cache_Exception
+     */
+    private function saveToCache($data)
+    {
+        $cacheTags = [];
+        Mage::app()->getCache()->save(serialize($data), $this->getCacheId(), $cacheTags);
+    }
+
+    /**
+     * @param array $headerRow
+     * @return bool
+     */
+    private function validateHeaderRow(array $headerRow): bool
+    {
+        if (count($headerRow) !== count($this->columns)) {
+            throw new InvalidArgumentException('CSV header is of unexpected size: '.var_export($headerRow));
+        }
+
+        $i = 0;
+        foreach ($this->columns as $columnCode => $columnName) {
+            if ($headerRow[$i] != $columnName) {
+                throw new InvalidArgumentException(sprintf('CSV header contains unexpected value "%s" at position %d',
+                    $headerRow[$i], $i));
+            }
+            $i++;
+        }
+
+        return true;
     }
 }
